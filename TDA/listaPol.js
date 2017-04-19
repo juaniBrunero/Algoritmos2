96f302bba62cbc5739f47b691dba4bf9fc420a01
juaniloco <juanibrunero@gmail.com>
2017-04-19 17:26:47 -0300

    Primera parte

--- TDA/listaPol.js
+++ TDA/listaPol.js
@@ -1,3 +1,9 @@
+/*
+  Objeto de listaPol hace de lista parametrica para distintos tipos de datos por polimorfismo
+  funciones propias de las listas(simplemente encadenadas)
+  y una extra para mostrar en javascript usando p5.js
+*/
+
 function listaPol(){
   this.sig  = null;
   this.dato = null;
@@ -5,11 +11,11 @@ function listaPol(){
 
 //Proyectores
 
-listaPol.prototype.esVacia = function () {
+listaPol.prototype.esVacia = function () { // DEVUELVE BOOL SI ES VACIA
   return this.sig == null;
 };
 
-function agl(x, xs) {
+function agl(x, xs) { // AGREGA ADELANTE UN ELEMENTO
   var lis = new listaPol();
   lis.sig = xs;
   lis.dato = x; //USA POLIMORFISMO PARA EL DATO
@@ -30,7 +36,7 @@ listaPol.prototype.elCab = function (){ // ELMINA LA CABEZA DE LA LISTA
   return this.sig;
 };
 
-listaPol.prototype.copiar = function (){
+listaPol.prototype.copiar = function (){ // COPIA ESTA LISTA EN OTRA
   if(this.sig == null){
     return new listaPol();
   }else{
@@ -41,18 +47,35 @@ listaPol.prototype.copiar = function (){
 //Operaciones complejas
 
 listaPol.prototype.longitud = function () { // DEVUELVE LONG DE LA LISTA (iteracion)
-  var l = this;
-  var i = 0;
-  while(l.sig != null){
-    l = l.sig;
-    i++;
+  if(this.dato == null){
+    return 0;
+  }else{
+    return (this.sig.longitud() + 1);
   }
-  return i;
+};
+
+listaPol.prototype.concatenar = function(g){ // CONCATENA ESTA + OTRA
+  if(g.sig == null){
+    return this;
+  }else{
+    if(this.sig == null){
+      return g;
+    }else{
+      return agl(this.dato, this.sig.concatenar(g))
+    }
+  }
+};
+
+function aConTodos (a, xs){
+  if(xs.sig != null){
+    xs.dato = xs.dato * a;
+    aConTodos(a, xs.resto(xs));
+  }
 };
 
 //Operaciones para mostrar
 
-listaPol.prototype.mostrar = function (x, y, r) {
+listaPol.prototype.mostrar = function (x, y, r, t) { // MUESRTA CON 3 PARAMETROS
   if(this.sig != null){
     stroke(255);
     strokeWeight(1);
@@ -61,9 +84,9 @@ listaPol.prototype.mostrar = function (x, y, r) {
     textSize(r/2);
     text(this.dato, x - (r/8), y + (r/8));
     if(y+3/2 * r > height){
-      x += r;
-      y = 0;
+      x += t + r+10;
+      y = t;
     }
-    this.sig.mostrar(x, y + r, r);
+    this.sig.mostrar(x, y + r, r, t);
   }
 };
--- TDA/sketch.js
+++ TDA/sketch.js
@@ -1,15 +1,16 @@
 var l;
+var c;
 
 function setup() {
   createCanvas(400, 400);
   l = new listaPol();
-
+  c = new listaPol();
   rectMode(CENTER);
 }
 
 function draw(){
   background(0);
-  l.mostrar(50, 50, 50);
+  l.mostrar(50, 50, 50, 0);
 }
 
 function mousePressed(){
